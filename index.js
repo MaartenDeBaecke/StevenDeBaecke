707812c6658d62ae3965ef6728622b35ea7747b5
Maarten De Baecke <maartendebaecke@gmail.com>
2023-02-10 15:03:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function scrollAbout(){
+ var element = document.getElementById("about");
+ element.scrollIntoView();
+}
+
+
+function scrollContact(){
+ var element = document.getElementById("contact");
+ element.scrollIntoView();
+}
+
+function scrollCompositions(){
+ var element = document.getElementById("music");
+ element.scrollIntoView();
+}
+
+
+function scrollRigol(){
+ var element = document.getElementById("rigol");
+ element.scrollIntoView();
+}
+
